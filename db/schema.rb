f90f9e91c899fb613ed29261386737de8080634f
Pavel Golenkov <pashex@mail.ru>
2015-02-04 19:03:13 +0500

    Migrate to Postgres database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150203072725) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: true do |t|
     t.date     "release_date"
     t.string   "name"
